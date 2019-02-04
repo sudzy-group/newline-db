e55a1a3e499f5ec536ac622432f2d060ca0c0458
Roy Ganor <ganoro@gmail.com>
2019-02-04 14:42:03 +0200

    summary tables

--- etl/summary_tables.sql
+++ etl/summary_tables.sql
@@ -1,15 +1,43 @@
 ########################
-# New customers count
+# Items Sales
 ########################
--- DROP TABLE IF EXISTS `{{store_id}}_customers_count`;
+DROP TABLE IF EXISTS `ItemsSales`;
 
--- CREATE TABLE `{{store_id}}_customers_count` (
---   `id` int(11) NOT NULL AUTO_INCREMENT,
---   `day` date NOT NULL,
---   `customers_count` INT NULL,
---   PRIMARY KEY (`id`)
--- );
+CREATE TABLE `ItemsSales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `dishId` varchar(250) NOT NULL,
+  `hour` varchar(8) NOT NULL,
+  `sold` INT(11) NULL DEFAULT NULL,
+  `gross` DECIMAL(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
--- INSERT INTO `{{store_id}}_customers_count` (day, customers_count)
--- SELECT date(FROM_UNIXTIME(created_at/1000)) as day, count(date(FROM_UNIXTIME(created_at/1000))) as customers_count FROM `{{store_id}}_customers` GROUP BY DATE(CONVERT_TZ(FROM_UNIXTIME(created_at/1000),'+00:00','-04:00'));
+INSERT INTO `ItemsSales` (userId, category, name, dishId, hour, sold, gross)
+SELECT min(oi.userId) as userId, min(d.categoryId) as category, min(d.dishName) as name, min(oi.dishId) as dishId, DATE_FORMAT(FROM_UNIXTIME(min(oi.createdAt)/1000), "%y%m%d%H") as hour, count(*) as sold, sum(price) as gross FROM `OrderItems` as oi
+LEFT JOIN Dishes AS d ON oi.dishId = d.dishId
+GROUP BY HOUR(FROM_UNIXTIME(oi.createdAt/1000)), oi.dishId, oi.userId
+ORDER BY hour;
 
+########################
+# Modifiers Sales
+########################
+DROP TABLE IF EXISTS `ModifiersSales`;
+
+CREATE TABLE `ModifiersSales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `name` varchar(250) NULL,
+  `hour` varchar(8) NOT NULL,
+  `sold` INT(11) NULL DEFAULT NULL,
+  `gross` DECIMAL(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `ModifiersSales` (userId, name, hour, sold, gross)
+SELECT min(oic.userId) as userId, min(c.choiceName) as name, DATE_FORMAT(FROM_UNIXTIME(min(oic.createdAt)/1000), "%y%m%d%H") as hour, count(*) as sold, sum(price) as gross FROM `OrderItemChoices` as oic
+LEFT JOIN Choices AS c ON oic.choiceId = c.choiceId
+GROUP BY HOUR(FROM_UNIXTIME(oic.createdAt/1000)), c.choiceName, oic.userId
+ORDER BY hour;
