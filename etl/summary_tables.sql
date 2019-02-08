cdc3e60d555a113bbd9ba9cb6a4536325f81f895
Roy Ganor <ganoro@gmail.com>
2019-02-08 23:15:56 +0200

    sum (q) * price

--- etl/summary_tables.sql
+++ etl/summary_tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE `ItemsSales` (
 );
 
 INSERT INTO `ItemsSales` (userId, categoryName, category, name, dishId, milisec, hour, sold, gross)
-SELECT min(oi.userId) as userId, min(c.categoryName) as categoryName, min(d.categoryId) as category, min(d.dishName) as name, min(oi.dishId) as dishId, min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000) as milisec, FROM_UNIXTIME((min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000))/1000) as hour, sum(quantity) as sold, sum(quantity) * sum(price) as gross FROM `OrderItems` as oi
+SELECT min(oi.userId) as userId, min(c.categoryName) as categoryName, min(d.categoryId) as category, min(d.dishName) as name, min(oi.dishId) as dishId, min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000) as milisec, FROM_UNIXTIME((min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000))/1000) as hour, sum(quantity) as sold, sum(quantity) * price as gross FROM `OrderItems` as oi
 LEFT JOIN Dishes AS d ON oi.dishId = d.dishId
 LEFT JOIN Categories AS c ON d.categoryId = c.categoryId
 GROUP BY DATE_FORMAT(FROM_UNIXTIME(oi.createdAt/1000), "%Y%m%d"), oi.dishId, oi.userId
@@ -41,7 +41,7 @@ CREATE TABLE `ModifiersSales` (
 );
 
 INSERT INTO `ModifiersSales` (userId, name, milisec, hour, sold, gross)
-SELECT min(oic.userId) as userId, min(c.choiceName) as name, min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000) as milisec, FROM_UNIXTIME((min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000))/1000) as hour, sum(quantity) as sold, sum(quantity) * sum(price) as gross FROM `OrderItemChoices` as oic
+SELECT min(oic.userId) as userId, min(c.choiceName) as name, min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000) as milisec, FROM_UNIXTIME((min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000))/1000) as hour, sum(quantity) as sold, sum(quantity) * price as gross FROM `OrderItemChoices` as oic
 LEFT JOIN Choices AS c ON oic.choiceId = c.choiceId
 GROUP BY DATE_FORMAT(FROM_UNIXTIME(oic.createdAt/1000), "%Y%m%d"), c.choiceName, oic.userId
 ORDER BY hour;
