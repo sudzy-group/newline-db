0a4ce9a6dd2a06f8bae896871ab27b2736d27f76
Roy Ganor <ganoro@gmail.com>
2021-05-19 10:38:34 -0400

    fix  convert zone

--- etl/summary_tables.sql
+++ etl/summary_tables.sql
@@ -18,7 +18,7 @@ CREATE TABLE `ItemsSales` (
 );
 
 INSERT INTO `ItemsSales` (userId, categoryName, category, name, dishId, milisec, hour, sold, gross)
-SELECT min(oi.userId) as userId, min(c.categoryName) as categoryName, min(d.categoryId) as category, min(oi.name) as name, min(oi.dishId) as dishId, min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000) as milisec, DATE(CONVERT_TZ(FROM_UNIXTIME((min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000))/1000), '+00:00', '-13:00')) as hour, sum(quantity) as sold, sum(quantity) * min(price) as gross FROM `OrderItems` as oi
+SELECT min(oi.userId) as userId, min(c.categoryName) as categoryName, min(d.categoryId) as category, min(oi.name) as name, min(oi.dishId) as dishId, min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000) as milisec, CONVERT_TZ(FROM_UNIXTIME((min(oi.createdAt) - mod(min(oi.createdAt), 60 * 60 * 1000))/1000), '+00:00', '-12:00') as hour, sum(quantity) as sold, sum(quantity) * min(price) as gross FROM `OrderItems` as oi
 LEFT JOIN Dishes AS d ON oi.dishId = d.dishId
 LEFT JOIN Categories AS c ON d.categoryId = c.categoryId
 GROUP BY oi.createdAt - mod(oi.createdAt, 60 * 60 * 1000), oi.dishId, oi.userId
@@ -41,7 +41,7 @@ CREATE TABLE `ModifiersSales` (
 );
 
 INSERT INTO `ModifiersSales` (userId, name, milisec, hour, sold, gross)
-SELECT min(oic.userId) as userId, min(oic.name) as name, min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000) as milisec, DATE(CONVERT_TZ(FROM_UNIXTIME((min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000))/1000), '+00:00', '-13:00')) as hour, sum(quantity) as sold, sum(quantity) * min(price) as gross FROM `OrderItemChoices` as oic
+SELECT min(oic.userId) as userId, min(oic.name) as name, min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000) as milisec, CONVERT_TZ(FROM_UNIXTIME((min(oic.createdAt) - mod(min(oic.createdAt), 60 * 60 * 1000))/1000), '+00:00', '-12:00') as hour, sum(quantity) as sold, sum(quantity) * min(price) as gross FROM `OrderItemChoices` as oic
 LEFT JOIN Choices AS c ON oic.choiceId = c.choiceId
 GROUP BY oic.createdAt - mod(oic.createdAt, 60 * 60 * 1000), c.choiceName, oic.userId
 ORDER BY hour;
